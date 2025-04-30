8742c6152fe1cff0ad5940ba823c5e7ac96d4111
Ahameedx <160577703+Ahameedx07@users.noreply.github.com>
2025-05-01 01:05:47 +0600

    Add files via upload

---
+++ firebase-init.js
@@ -0,0 +1,15 @@
+
+// Initialize Firebase
+const firebaseConfig = {
+  apiKey: "YOUR_API_KEY",
+  authDomain: "your-app.firebaseapp.com",
+  projectId: "your-app-id",
+  storageBucket: "your-app.appspot.com",
+  messagingSenderId: "your-messaging-sender-id",
+  appId: "your-app-id"
+};
+
+// Initialize Firebase App and Services
+firebase.initializeApp(firebaseConfig);
+const auth = firebase.auth();
+const db = firebase.firestore();
